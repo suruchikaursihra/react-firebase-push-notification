3a0240ffd8dda865fdf4695b2906f3bcc3d01b44
suruchi kaur <31774267+suruchikaursihra@users.noreply.github.com>
2020-03-31 01:49:07 +0530

    Update init-fcm.js

--- src/init-fcm.js
+++ src/init-fcm.js
@@ -2,14 +2,14 @@ import * as firebase from "firebase/app";
 import "firebase/messaging";
 const initializedFirebaseApp = firebase.initializeApp({
     // Project Settings => Add Firebase to your web app
-    messagingSenderId: "1092757106873",
-    projectId: "react-firebase-authentication0",
-    apiKey: "AIzaSyAExOetRPfUIuSyuv1SXwzwOjMeYQBuUXU",
-    appId: "1:1092757106873:web:a58dac8303dda7c2"
+    messagingSenderId: "109....6873",
+    projectId: "reac....tication",
+    apiKey: "AIz....MeYQBu.....UXU",
+    appId: "1:10....dda7......c2"
 });
 const messaging = initializedFirebaseApp.messaging();
 messaging.usePublicVapidKey(
     // Project Settings => Cloud Messaging => Web Push certificates
-    "BDmb1xojnKXD0m1z0jIb1m4LuFLDVSQlXls7-A_TtSZAVI86-glxlIJhL05j4z76K5xmHVoFmkO5y0-X97Hask4"
+    "BDmb.......86-glxlI........Hask4"
 );
 export { messaging };
